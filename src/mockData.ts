0604616cbad1851f49583a749a9f563b0480de48
victor <mbarachivictor@gmail.com>
2024-07-21 10:52:19 +0100

    Displaying the list items using the mockData

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,6 +1,5 @@
 import { Payslip } from "./types";
 
-
 const payslips: Payslip[] = [
     { id: '1', fromDate: '1672531200', toDate: '1675209600', file: '/assets/sample1.pdf' },
     { id: '2', fromDate: '1675209600', toDate: '1677628800', file: '/assets/sample2.pdf' },
@@ -8,3 +7,5 @@ const payslips: Payslip[] = [
     { id: '4', fromDate: '1680307200', toDate: '1682899200', file: '/assets/sample4.pdf' },
     { id: '5', fromDate: '1682899200', toDate: '1685577600', file: '/assets/sample5.pdf' },
 ]
+
+export default payslips
--- src/pages/PayslipList.tsx
+++ src/pages/PayslipList.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
+import payslipData from '../mockData'
+import { Payslip } from "../types"
 
 const PayslipList = () => {
-  return (
-    <div>PayslipList</div>
-  )
+    const [payslips, setPayslips] = useState<Payslip[]>(payslipData)
+
+    const formatDate = (timestamp: string) => {
+        const date = new Date(parseInt(timestamp) * 1000);
+        return date.toLocaleDateString();
+    };
+
+    return (
+        <div>
+            <h1>Payslip List</h1>
+            <ul>
+                {payslips.map((paylsip => (
+                    <li key={paylsip.id}>
+                        {formatDate(paylsip.fromDate)} to {formatDate(paylsip.toDate)}
+                    </li>
+                )))}
+            </ul>
+        </div>
+    )
 }
 
 export default PayslipList
